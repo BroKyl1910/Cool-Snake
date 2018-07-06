c19d66a5fd98e8f1f2af46bc52356bd9c99b19ec
brookies1910 <brooks.kyle621@gmail.com>
2018-07-07 01:08:56 +0200

    Refactor
    Refactored, broken, game over shows before game starts

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 /**
- * A simple snake game.
- */
+* A simple snake game.
+*/
 
 var snake = [];
 var direction;
@@ -12,18 +12,18 @@ function setup() {
 	frameRate(15);
 	createCanvas(400, 400);
 	direction = createVector(0, 0);
-	makeFood();
-	prev = millis();
+	moveFood();
 	ignoreInput = false;
-	snake.push(createVector(200, 200))
+	snake.push(createVector(200, 200));
+	score = 0;
 }
 
-function makeFood() {
+function moveFood() {
 	do {
 		food = p5.Vector.random2D().mult(400);
 		food.x = Math.floor(abs(food.x) / 10) * 10;
 		food.y = Math.floor(abs(food.y) / 10) * 10;
-	} while (snake.some(block => block.x == food.x && block.y == food.y ) || food.x==0 || food.x==canvas.width || food.y==0 || food.y==canvas.height)
+	} while (snake.some(block => block.x == food.x && block.y == food.y ) || food.x<=10 || food.x>=canvas.width-10 || food.y<=10 || food.y>=canvas.height-10);
 }
 
 function keyPressed() {
@@ -43,62 +43,83 @@ function keyPressed() {
 	ignoreInput = true;	/* we have to wait until next frame rate to be . */
 }
 
-function updatePosition() {
-	/**
-	 * Start from the tail, make the position of every 
-	 * block the same as the one ahead of it.
-	 */
-	for (var i = snake.length - 1; i > 0; i--) {
-		snake[i].x = snake[i - 1].x;
-		snake[i].y = snake[i - 1].y;
-	}
-	
-	/* move the head accordingly. */
-	snake[0].x += 10 * direction.x;
-	snake[0].y += 10 * direction.y;
-}
-
 function ateFood() {
 	return food.x === snake[0].x && food.y === snake[0].y;
 }
 
 function draw() {
-	if(gameOver()) {
-		textSize(32);
-		fill(255, 0, 0);
-		text('Game Over', 117, 200);
-		return;
-	}
-		
+	fill(0);
 	background(0);
-	fill(255);
+	update();
+}
+
+function update(){
+	drawFood();
+	moveSnake();
+	if (ateFood()) {
+		addBlockToSnake();
+		score++;
+		moveFood();
+	}
+	drawSnake();
+	drawScore();
+	if(snakeOutOfBounds){
+		gameOver();
+	}
+	ignoreInput = false;		
+}
+function moveSnake() {
+/**
+* Start from the tail, make the position of every 
+* block the same as the one ahead of it.
+*/
+for (var i = snake.length - 1; i > 0; i--) {
+	snake[i].x = snake[i - 1].x;
+	snake[i].y = snake[i - 1].y;
+}
+
+/* move the head accordingly. */
+snake[0].x += 10 * direction.x;
+snake[0].y += 10 * direction.y;
+}
+function drawFood(){
+	fill(255,255,0);
 	rect(food.x, food.y, 10, 10);
+	fill(255);
+}
+
+function drawSnake(){
 	snake.forEach(block => {
 		rect(block.x, block.y, 10, 10);
 	});
-	
-	updatePosition();
-	
-	if (ateFood()) {
-		var head = snake[snake.length - 1];
-		var block = createVector(head.x + direction.x * 10, head.y + direction.y * 10);
-		snake.push(block);
-		score++;
-		makeFood();
-	}
-	drawScore();
-	ignoreInput = false;
 }
+
+function addBlockToSnake(){
+	var head = snake[snake.length - 1];
+	var block = createVector(head.x + direction.x * 10, head.y + direction.y * 10);
+	snake.push(block);
+}
+
+
 function gameOver(){
-	if(snakeOutOfBounds()) return true;
+	textSize(32);
+	fill(255, 0, 0);
+	text('Game Over', 117, 200);
+	window.stop();
 }
 function snakeOutOfBounds(){
-	return snake[0].x >= canvas.width || snake[0].x<0 || snake[0].y >= canvas.height || snake[0].y<0;
+	return  snake[0].x >= canvas.width || snake[0].x<0 || snake[0].y >= canvas.height || snake[0].y<0;
+}
+function snakeTouchingItself(){
+	for (var i = 1; i < snake.length; i++) {
+		if(snake[i].x === snake[0].x && snake[i].y === snake[0].y){
+			return true;
+		}
+	}
+	return false;
 }
 function drawScore(){
-	push();
 	fill(0, 255, 0);
 	textSize(18);
 	text('Score: '+score, 20, 20);
-	pop();
 }
